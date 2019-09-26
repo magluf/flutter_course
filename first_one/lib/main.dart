2175282912876a35c5563689e73da1df3ac7f072
magluf <sergio.magluf@accenture.com>
2019-09-26 17:06:03 -0300

    Widget for questions.

--- first_one/lib/main.dart
+++ first_one/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -30,13 +32,21 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(
+            Question(
               questions[questionIndex],
             ),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: _answerQuestion,
-            )
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: _answerQuestion,
+            ),
           ],
         ),
       ),
