581b267a9d9b2358649d9ae5f7aa578c5c216fe6
magluf <sergio.magluf@accenture.com>
2019-09-26 15:42:40 -0300

    Stateless widget.

--- first_one/lib/main.dart
+++ first_one/lib/main.dart
@@ -2,15 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favourite colour?",
+      "What's your favourite animal?",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App booyah'),
         ),
-        body: Text('Body text'),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            )
+          ],
+        ),
       ),
     );
   }
