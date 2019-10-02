51c02656c74e36f56bbcfcf1f67028e87d67184d
Magluf <sergio.magluf@accenture.com>
2019-10-02 15:33:04 -0300

    Step 2 of assignment DONE!

--- assignment_1/lib/main.dart
+++ assignment_1/lib/main.dart
@@ -8,14 +8,37 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  var _text = 'HI!';
+
+  void changeText(String newText) {
+    setState(() {
+      _text = newText;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App booyah'),
+          title: Text('First Assignment'),
         ),
-        body: Text('hi'),
+        body: Column(
+          children: [
+            Text(_text),
+            Container(
+              width: double.infinity,
+              child: RaisedButton(
+                color: Colors.blue,
+                textColor: Colors.white,
+                child: Text('Press me, bitch.'),
+                onPressed: () {
+                  changeText('BYE!');
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
